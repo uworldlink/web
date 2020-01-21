c692e32d229fec1b3345161a4ad8273e922497fd
uworldlink <59925342+uworldlink@users.noreply.github.com>
2020-01-21 19:28:00 +0700

    Update skin-switch.js

--- azda website/components/skin-switch.js
+++ azda website/components/skin-switch.js
@@ -24,18 +24,18 @@ $(document).ready(function() {
   	e.preventDefault();
   	localStorage.removeItem('themeSwitch');
     $('#skin').attr("href", auto_theme);
-    themeSelect(auto_mode);
+    themeSelect('auto_mode');
 	});
   $('.light-button').click(function(e) {
   	e.preventDefault();
     localStorage.setItem('themeSwitch', 1);
     $('#skin').attr("href", light_theme);
-    themeSelect(light_mode);
+    themeSelect('light_mode');
 	});
   $('.dark-button').click(function(e) {
   	e.preventDefault();
   	localStorage.setItem('themeSwitch', 2);
     $('#skin').attr("href", dark_theme);
-    themeSelect(dark_mode);
+    themeSelect('dark_mode');
 	});
 });
