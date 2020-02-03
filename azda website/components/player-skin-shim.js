c5b9d8a7501432f84dee191ce26c1f435bd71dd8
uworldlink <59925342+uworldlink@users.noreply.github.com>
2020-02-03 17:42:43 +0700

    Update player-skin-shim.js

--- azda website/components/player-skin-shim.js
+++ azda website/components/player-skin-shim.js
@@ -13,7 +13,7 @@ function load_player_CSS(ref) {
 }
 var auto_skin_player = 'https://rawcdn.githack.com/uworldlink/web/db61e0b5e9afdaa6744fcfa84fd102729b89112c/azda website/css/default.css';
 var light_skin_player = 'https://rawcdn.githack.com/uworldlink/web/db61e0b5e9afdaa6744fcfa84fd102729b89112c/azda website/css/light-theme.css';
-var dark_skin_player = 'https://rawcdn.githack.com/uworldlink/web/5032c4f6a3c2417cacba931c12bc95aebba09c88/azda website/css/dark-theme.css';
+var dark_skin_player = 'https://rawcdn.githack.com/uworldlink/web/b010f8fb25a0f648d97f6555340fced63b742efc/azda website/css/dark-skin-player.css';
 
 if (localStorage.getItem('themeSwitch') === null) {
   load_player_CSS(auto_skin_player);
