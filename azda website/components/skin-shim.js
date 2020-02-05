a820ef6d90aad2c2c0d47329794ba2e73a24bf32
uworldlink <59925342+uworldlink@users.noreply.github.com>
2020-02-05 18:21:20 +0700

    Update skin-shim.js

--- azda website/components/skin-shim.js
+++ azda website/components/skin-shim.js
@@ -11,8 +11,8 @@ function loadCSS(ref) {
   head.appendChild(link);
   }
 }
-var auto_theme = 'https://rawcdn.githack.com/uworldlink/web/db61e0b5e9afdaa6744fcfa84fd102729b89112c/azda website/css/default.css';
-var light_theme = 'https://rawcdn.githack.com/uworldlink/web/db61e0b5e9afdaa6744fcfa84fd102729b89112c/azda website/css/light-theme.css';
+var auto_theme = 'https://rawcdn.githack.com/uworldlink/web/67012c6bfd2ecb51f5d4dc97533234b9209b92bf/azda website/css/default.css';
+var light_theme = 'https://rawcdn.githack.com/uworldlink/web/67012c6bfd2ecb51f5d4dc97533234b9209b92bf/azda website/css/light-theme.css';
 var dark_theme = 'https://rawcdn.githack.com/uworldlink/web/5032c4f6a3c2417cacba931c12bc95aebba09c88/azda website/css/dark-theme.css';
 
 if (localStorage.getItem('themeSwitch') === null) {
