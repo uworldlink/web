c4a516f2b1d17dab8d1ac5e9d86f471bb87e2ffb
uworldlink <59925342+uworldlink@users.noreply.github.com>
2020-02-07 17:27:31 +0700

    Update scroll-lock.js

--- azda website/components/scroll-lock.js
+++ azda website/components/scroll-lock.js
@@ -2,25 +2,31 @@
 var targetMenu = document.querySelector("#overlay");
 var targetModal = document.querySelector("#modal");
 
-$(document).ready(function() {
- var clicked = "false";
-  $('.w-nav-button').click(function(e) {
-    e.preventDefault();
-    if (clicked == "false") {
-    bodyScrollLock.lock(targetMenu);
-    clicked = "true";
-    } else {
-    bodyScrollLock.unlock(targetMenu);
-    clicked = "false";
+var $div = $("#navmenu");
+var observer = new MutationObserver(function(mutations) {
+  mutations.forEach(function(mutation) {
+    if (mutation.attributeName === "class") {
+      //var attributeValue = $(mutation.target).prop(mutation.attributeName);
+      //console.log("Class attribute changed to:", attributeValue);
+      if (mutation.target.classList.contains('w--open')) {
+        bodyScrollLock.lock(targetMenu);
+      } else {
+        bodyScrollLock.unlock(targetMenu);
+      }
     }
   });
 });
+observer.observe($div[0], {
+  attributes: true
+});
 
-$('.modal-open').click(function(e) {
+$(document).ready(function() {
+ $('.modal-open').click(function(e) {
   e.preventDefault();
   bodyScrollLock.lock(targetModal);
-});
-$('.modal-close').click(function(e) {
+ });
+ $('.modal-close').click(function(e) {
   e.preventDefault();
   bodyScrollLock.unlock(targetModal);
+ });
 });
